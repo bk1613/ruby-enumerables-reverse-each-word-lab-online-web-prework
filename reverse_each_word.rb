15dbb7d5c99a4bdfcd1e924a3d8d4d44f757e1a7
Brian Kao <bk1613@nyu.edu>
2019-09-05 20:49:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   
   string_array = string.split(" ")
-  reversearr = string_array.collect do |word|
+  reversearr = string_array.collect {do} |word|
     word.reverse
   end
   reversearr
